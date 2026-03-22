93faad48e00f0fa5064add0b34bffdac02501f84
AI-Tony <119665632+AI-Tonny@users.noreply.github.com>
2026-03-22 22:07:43 +0200

    setting base project

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
-import { Text, View } from "react-native";
+import { View, Text } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
+    <View style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello, World!</Text>
+        justifyContent: 'center',
+        alignItems: 'center'
+    }}>
+        <Text>Hello, world</Text>
     </View>
   );
 }
